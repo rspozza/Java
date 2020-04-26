c298789b46b65dfc566ffddd1d9e2030978ef8eb
Rogerio Pozza <rspozza@yahoo.com.br>
2020-04-26 10:58:09 -0300

    LImpeza de codigo

--- Revisao/Exemplo/src/utfpr/banco/ContaCorrente.java
+++ Revisao/Exemplo/src/utfpr/banco/ContaCorrente.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utfpr.banco;
 
-/**
- *
- * @author pozza
- */
 public class ContaCorrente implements Conta{
 
     private int valor = 0;
